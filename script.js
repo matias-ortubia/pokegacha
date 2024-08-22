aefe72a1fa9a5052077faa3a7b3f232080336335
matias-ortubia <matiortubia@gmail.com>
2024-08-22 02:09:51 -0300

    Add: styles and counter for obtained pkm

--- script.js
+++ script.js
@@ -5,18 +5,25 @@ const MAX_POKEMON = 150;
 const multiPullButton = document.getElementById("multiPullButton");
 const singlePullButton = document.getElementById("singlePullButton");
 const showObtainedPokemonButton = document.getElementById("showObtainedPokemonButton");
-const pokemonContainer = document.getElementById("pokemonContainer");
-const obtainedPokemon = [];
+const pkmListContainer = document.getElementById("pkmListContainer");
+const obtainedPokemonList = [];
 
 const getRandomPokemon = async () => {
     const randomNum = Math.floor(Math.random() * MAX_POKEMON) + 1;
     const url = "https://pokeapi.co/api/v2/pokemon/" + randomNum.toString();
-    const pokemon = await fetch(url).then(response => response.json())
-    obtainedPokemon.push(pokemon);
+    const obtainedPokemon = await fetch(url).then(response => response.json());
+    const alreadyObtainedPkm = obtainedPokemonList.find(pkm => pkm.data.name.toLowerCase() === obtainedPokemon.name.toLowerCase());
+    const pokemon = alreadyObtainedPkm ?? {data: obtainedPokemon, quantity: 1};
+    if (alreadyObtainedPkm) {
+        pokemon.quantity += 1;
+        alreadyObtainedPkm.quantity += 1;
+    } else {
+        obtainedPokemonList.push({...pokemon});
+    }
     return pokemon;
 };
 
-const getRandomPokemons = async () => {
+const getRandomPokemonList = async () => {
     const pokemonList = [];
     for (let i = 0; i < 10; i++) {
         const pokemon = await getRandomPokemon();
@@ -25,37 +32,53 @@ const getRandomPokemons = async () => {
     return pokemonList;
 };
 
-const renderPokemon = (name) => {
-    const newPokemonElement = document.createElement("p");
-    newPokemonElement.innerHTML = name;
-    pokemonContainer.appendChild(newPokemonElement);
+const renderPokemon = (pokemon) => {
+    const newPokemonContainer = document.createElement("div");
+    newPokemonContainer.className = "pkmContainer";
+
+    const newPokemonName = document.createElement("h5");
+    newPokemonName.className = "pkmName";
+    newPokemonName.innerHTML = pokemon.data.name + " x" + pokemon.quantity;
+    newPokemonContainer.appendChild(newPokemonName);
+
+    const newPokemonImg = document.createElement("img");
+    newPokemonImg.className = "pkmImg";
+    newPokemonImg.src = pokemon.data.sprites.front_default;
+    newPokemonContainer.appendChild(newPokemonImg);
+
+    pkmListContainer.appendChild(newPokemonContainer);
 };
 
-const renderPokemons = (pokemonList) => {
-    pokemonList.forEach(pokemon => renderPokemon(pokemon.name));
+const renderPokemonList = (pokemonList) => {
+    pokemonList.forEach(pokemon => renderPokemon(pokemon));
 };
 
 const handleSinglePull = () => {
-    clearPulledPokemon();
-    getRandomPokemon().then(pokemon => renderPokemon(pokemon.name));
+    clearShownPokemon();
+    getRandomPokemon().then(pokemon => renderPokemon(pokemon));
 };
 
 const handleMultiPull = () => {
-    clearPulledPokemon();
-    getRandomPokemons().then(pokemonList => renderPokemons(pokemonList));
+    clearShownPokemon();
+    getRandomPokemonList().then(pokemonList => renderPokemonList(pokemonList));
 };
 
-const clearPulledPokemon = () => {
-    while(pokemonContainer.firstChild) {
-        pokemonContainer.removeChild(pokemonContainer.lastChild);
+const clearShownPokemon = () => {
+    while(pkmListContainer.firstChild) {
+        pkmListContainer.removeChild(pkmListContainer.lastChild);
     }
 };
 
 const showObtainedPokemon = () => {
-    console.log(obtainedPokemon);
+    clearShownPokemon();
+    obtainedPokemonList.forEach(pokemon => renderPokemon(pokemon));
 };
 
 singlePullButton.addEventListener("click", handleSinglePull);
 multiPullButton.addEventListener("click", handleMultiPull);
 showObtainedPokemonButton.addEventListener("click", showObtainedPokemon);
+
+// Para probar, borrar despues
+handleMultiPull();
+
 
